1f9c3e319bb8a82d71fc4892bd14ac47e5ab4192
Zehra Coşkun <110024096+zehraCoskun@users.noreply.github.com>
2024-05-02 17:55:10 +0300

    web view project created

---
+++ WebViewProject/WebViewProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebViewProject
+//
+//  Created by Zehra Coşkun on 2.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
