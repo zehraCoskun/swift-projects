659fcaa21c82301474d54725cdfa5a3b63e375c7
Zehra Coşkun <110024096+zehraCoskun@users.noreply.github.com>
2024-05-06 14:30:43 +0300

    petitions projects created

---
+++ Petitions/Petitions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Petitions
+//
+//  Created by Zehra Coşkun on 6.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
