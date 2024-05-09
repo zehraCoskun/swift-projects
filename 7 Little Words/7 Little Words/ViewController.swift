0dfec438c4fcfb7dba9dc30fbce8842543f66b9a
Zehra Coşkun <110024096+zehraCoskun@users.noreply.github.com>
2024-05-09 15:54:08 +0300

    7 Little Words created

---
+++ 7 Little Words/7 Little Words/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  7 Little Words
+//
+//  Created by Zehra Coşkun on 9.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
