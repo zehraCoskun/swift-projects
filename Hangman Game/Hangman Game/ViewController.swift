24928c2f9140c5901f860f987e71b918fc25afd4
Zehra Coşkun <110024096+zehraCoskun@users.noreply.github.com>
2024-05-12 23:26:05 +0300

    text field added and labels text updated

--- Hangman Game/Hangman Game/ViewController.swift
+++ Hangman Game/Hangman Game/ViewController.swift
@@ -11,7 +11,14 @@ class ViewController: UIViewController {
 
     var titleLabel: UILabel!
     var charLabels = [UILabel]()
+    var answerTextField : UITextField!
+    var charViews : UIView!
     
+    
+    var allWords = [String]()
+    var selectedWord : String! = "hello"
+    var answerChars = [Character]()
+    var selectedChars = [Character]()
     
     override func loadView() {
         view = UIView()
@@ -24,9 +31,21 @@ class ViewController: UIViewController {
         titleLabel.tintColor = .systemGray
         view.addSubview(titleLabel)
         
-        let charViews = UIView()
+        charViews = UIView()
         charViews.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(charViews)
+        
+        answerTextField = UITextField()
+        answerTextField.translatesAutoresizingMaskIntoConstraints = false
+        answerTextField.placeholder = "Enter your answer"
+        answerTextField.font = UIFont.systemFont(ofSize: 18)
+        answerTextField.layer.borderWidth = 0.5
+        answerTextField.layer.borderColor = UIColor.systemGray.cgColor
+        answerTextField.layer.cornerRadius = 6.0
+        if let answerTextFieldText =  answerTextField.text{
+            answerChars = Array(answerTextFieldText)
+        }
+        view.addSubview(answerTextField)
         
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
@@ -34,34 +53,71 @@ class ViewController: UIViewController {
             
             charViews.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
             charViews.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            charViews.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9)
+            charViews.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+            
+            //answerTextField.topAnchor.constraint(equalTo: charViews.bottomAnchor, constant: 8),
+            answerTextField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+            answerTextField.heightAnchor.constraint(equalToConstant: 32),
+            answerTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            answerTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        
 
         ])
+
+
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        closeKeyboard()
+        getWords()
+        setCharViews()
+        
+    }
+    
+    func setCharViews(){
+        let buttonSpacing: CGFloat = 2.0
         let screenWidth = UIScreen.main.bounds.width
-        let width = (screenWidth - 40)/5
-        let height = (screenWidth - 40)/5
-                for row in 0..<5 {
+        let width = (screenWidth - 50)/5
+        let height = (screenWidth - 40)/10
+                for row in 0..<7 {
                     for col in 0..<5 {
                         let charLabel = UILabel()
-                        charLabel.layer.backgroundColor = UIColor.red.cgColor
+                        charLabel.layer.backgroundColor = UIColor.white.cgColor
                         charLabel.font = UIFont.systemFont(ofSize: 18)
-                        let frame = CGRect(x: CGFloat(col) * width, y: CGFloat(row) * height, width: width, height: height)
+                        charLabel.text = String(selectedChars[col])
+                        charLabel.textAlignment = .center
+                        let x = CGFloat(col) * (width + buttonSpacing)
+                                let y = CGFloat(row) * (height + buttonSpacing)
+                                let frame = CGRect(x: x, y: y, width: width, height: height)
                         charLabel.frame = frame
-                        charLabel.layer.cornerRadius = 12.0
-                        charLabel.layer.borderWidth = 1.0
-                        charLabel.layer.borderColor = UIColor.darkGray.cgColor
+                        charLabel.layer.cornerRadius = 4.0
+                        charLabel.layer.borderWidth = 0.5
+                        charLabel.layer.borderColor = UIColor.systemGray.cgColor
                         charViews.addSubview(charLabel)
                         charLabels.append(charLabel)
                     }
                 }
     }
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    func getWords(){
+        if let startWordsURL = Bundle.main.url(forResource: "hangman", withExtension: "txt") {
+                    if let startWords = try? String(contentsOf: startWordsURL){
+                        allWords = startWords.components(separatedBy: "\n")
+                    }
+            selectedWord = allWords.randomElement()
+            selectedChars = Array(selectedWord)                }
     }
-
+    
+    func closeKeyboard(){
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        view.isUserInteractionEnabled = true
+        view.addGestureRecognizer(tapGesture)
+    }
+    @objc func handleTap() {
+        view.endEditing(true)
+    }
 
 }
 
