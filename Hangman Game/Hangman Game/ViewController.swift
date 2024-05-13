b29d2eb268b5df8185c22247403d373d92a285d1
Zehra Coşkun <110024096+zehraCoskun@users.noreply.github.com>
2024-05-13 16:14:31 +0300

    green and yellow labels added

--- Hangman Game/Hangman Game/ViewController.swift
+++ Hangman Game/Hangman Game/ViewController.swift
@@ -16,13 +16,12 @@ class ViewController: UIViewController {
     
     
     var allWords = [String]()
-    var selectedWord : String! = "hello"
+    var selectedWord : String! = "APPLE"
     var usersWords = ["     ","     ","     ","     ","     ","     ","     "]{
         didSet{
             setCharViews()
         }
     }
-    var selectedChars = [Character]()
     var count = 0
     
     override func loadView() {
@@ -73,17 +72,27 @@ class ViewController: UIViewController {
                 for row in 0..<7 {
                     for col in 0..<5 {
                         let charLabel = UILabel()
-                        charLabel.layer.backgroundColor = UIColor.white.cgColor
-                        charLabel.font = UIFont.systemFont(ofSize: 18)
-                        charLabel.text = String(usersWords[row].prefix(col+1).suffix(1)) //userWords dizisi güncellendikçe buranın güncellenmesini nasıl sağlarım
+                        charLabel.font = UIFont.systemFont(ofSize: 18, weight: .bold)
                         charLabel.textAlignment = .center
                         let x = CGFloat(col) * (width + buttonSpacing)
-                                let y = CGFloat(row) * (height + buttonSpacing)
-                                let frame = CGRect(x: x, y: y, width: width, height: height)
+                        let y = CGFloat(row) * (height + buttonSpacing)
+                        let frame = CGRect(x: x, y: y, width: width, height: height)
                         charLabel.frame = frame
                         charLabel.layer.cornerRadius = 4.0
                         charLabel.layer.borderWidth = 0.5
                         charLabel.layer.borderColor = UIColor.systemGray.cgColor
+                    
+                        let labelText = String(usersWords[row].prefix(col+1).suffix(1))
+
+                        if labelText == selectedWord!.prefix(col+1).suffix(1) {
+                            charLabel.layer.backgroundColor = UIColor.systemGreen.cgColor
+                       
+                        }
+                        else if(selectedWord.contains(labelText)){
+                            charLabel.layer.backgroundColor = UIColor.systemYellow.cgColor
+                           
+                        }
+                        charLabel.text = labelText
                         charViews.addSubview(charLabel)
                         charLabels.append(charLabel)
                     }
@@ -92,22 +101,42 @@ class ViewController: UIViewController {
     
     func getWords(){
         if let startWordsURL = Bundle.main.url(forResource: "hangman", withExtension: "txt") {
-                    if let startWords = try? String(contentsOf: startWordsURL){
-                        allWords = startWords.components(separatedBy: "\n")
-                    }
-            selectedWord = allWords.randomElement()
-            selectedChars = Array(selectedWord)                }
+            if let startWords = try? String(contentsOf: startWordsURL){
+                allWords = startWords.components(separatedBy: "\n")
+            }
+            selectedWord = allWords.randomElement()?.uppercased()
+        }
     }
     
-    func closeKeyboard(){
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
-        view.isUserInteractionEnabled = true
-        view.addGestureRecognizer(tapGesture)
+    func startGame(didWin : Bool){
+        if !didWin {
+            let ac = UIAlertController(title: "Game Over",
+                                       message: "Your word : \(selectedWord ?? "hello")",
+                                       preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Ok", style: .default))
+            present(ac, animated: true)
+        }
+        usersWords = ["     ","     ","     ","     ","     ","     ","     "]
+        selectedWord = allWords.randomElement()?.uppercased()
+        charViews.subviews.forEach { $0.removeFromSuperview() }
+        setCharViews()
+        count = 0
+        
     }
-    @objc func handleTap() {
-        view.endEditing(true)
+    
+    func checkAnswer(selectedWord : String, usersAnswer : String){
+        if selectedWord == usersAnswer {
+            let ac = UIAlertController(title: "Congratulations !", 
+                                       message: "That's the right word",
+                                       preferredStyle: .actionSheet)
+            ac.addAction(UIAlertAction(title: "Thank you <3", 
+                                       style: .default){[weak self] _ in
+                self?.startGame(didWin: true)
+            })
+            present(ac, animated: true)
+        }
     }
-
+    
     func setTitleLabel(){
         titleLabel = UILabel()
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
@@ -129,12 +158,13 @@ class ViewController: UIViewController {
         view.addSubview(answerTextField)
     }
     
-    func startGame(){
-        let ac = UIAlertController(title: "Game Over",
-                                   message: "Your word : \(selectedWord ?? "hello")",
-                                   preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "Ok", style: .default))
-        present(ac, animated: true)
+    func closeKeyboard(){
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        view.isUserInteractionEnabled = true
+        view.addGestureRecognizer(tapGesture)
+    }
+    @objc func handleTap() {
+        view.endEditing(true)
     }
 
 }
@@ -142,7 +172,8 @@ extension ViewController : UITextFieldDelegate{
     //enter tuşuna basıldığında kullanılan metod
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         
-        if let answerTextFieldText =  answerTextField.text{
+        if var answerTextFieldText =  answerTextField.text?.uppercased(){
+            answerTextFieldText = answerTextFieldText.components(separatedBy: .whitespaces).joined()
             if answerTextFieldText.count != 5{
                 let ac = UIAlertController(title: "Opss!", 
                                            message: "You must write a word of only 5 letters",
@@ -152,12 +183,11 @@ extension ViewController : UITextFieldDelegate{
             } else if answerTextFieldText.count == 5 {
                 usersWords.insert(answerTextFieldText, at: count)
                 usersWords.remove(at: count+1)
+                checkAnswer(selectedWord: selectedWord, usersAnswer: answerTextFieldText)
                 count += 1
-                print(count)
                 if count == 7 {
-                    startGame()
+                    startGame(didWin: false)
                 }
-                print(usersWords)
             }
         }
            textField.text = ""
